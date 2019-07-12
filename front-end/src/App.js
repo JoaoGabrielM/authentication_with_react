dafe38d5929f7bdd24cdc621cccbb5d8502f1f7e
João Gabriel Machado <joaogabriel013@gmail.com>
2019-07-12 15:55:45 -0300

    Removing the Signup access

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,20 +2,11 @@ import React from 'react';
 import './App.css';
 
 import Login from './Login';
-import Signup from './Signup';
 
 function App() {
   return (
     <div className="container">
-        <h1>Test Aplication</h1>
-        <div className="row">
-            <div className="col-md-6">
-                <Login/>
-            </div>
-            <div className="col-md-6">
-                <Signup/>
-            </div>
-        </div>
+        <Login/>
     </div>
   );
 }
